08857c61ef5dfcba2e3f3e1789b0e0d8f4251c77
AAR072 <aliakber@requity.ca>
2022-05-16 12:56:12 -0400

    Completed Input

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace tictactoe
         public static string gameState = "running";
         public static string chosenPositionString;
         public static int chosenPosition;
+        public static int turn = 1;
         static void Main(string[] args)
         {
             //  Initialize grid classes
@@ -25,9 +26,9 @@ namespace tictactoe
             grid bottomLeft = new grid(7, false, false);
             grid bottomMiddle = new grid(8, false, false);
             grid bottomRight = new grid(9, false, false);
-
             // This is for debugging and controlling the game state
             grid masterGrid = new grid(999, false, false);
+
             // Instructions
             Console.WriteLine("Each grid has a number from 1 to 9 from left to right. Here is an example");
             Console.WriteLine("----------");
@@ -39,71 +40,376 @@ namespace tictactoe
             Console.WriteLine("----------");
             Thread.Sleep(1000);
 
+
             Console.WriteLine("Player 1, you are X. Go first");
 
             while (gameState.Equals("running"))
             {
+                if (turn == 1)
+                {
+                    Console.WriteLine("Player 1. Which box would you like to go?");
+                    chosenPositionString = Console.ReadLine();
+                    chosenPosition = Int32.Parse(chosenPositionString);
+                    Console.WriteLine(chosenPosition);
+                    // Ok some annoying if statements here we go....
+                    if (chosenPosition == 1)
+                    {
+                        if (!topLeft.occupied)
+                        {
+                            topLeft.value = 1;
+                            topLeft.occupied = true;
+                            turn = 2;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (topLeft.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                    if (chosenPosition == 2)
+                    {
+                        if (!topMiddle.occupied)
+                        {
+                            topMiddle.value = 1;
+                            topMiddle.occupied = true;
+                            turn = 2;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (topMiddle.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                    if (chosenPosition == 3)
+                    {
+                        if (!topRight.occupied)
+                        {
+                            topRight.value = 1;
+                            topRight.occupied = true;
+                            turn = 2;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (topRight.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                    if (chosenPosition == 4)
+                    {
+                        if (!middleLeft.occupied)
+                        {
+                            middleLeft.value = 1;
+                            middleLeft.occupied = true;
+                            turn = 2;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (middleLeft.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                    if (chosenPosition == 5)
+                    {
+                        if (!middleMiddle.occupied)
+                        {
+                            middleMiddle.value = 1;
+                            middleMiddle.occupied = true;
+                            turn = 2;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (middleMiddle.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                    if (chosenPosition == 6)
+                    {
+                        if (!middleRight.occupied)
+                        {
+                            middleRight.value = 1;
+                            middleRight.occupied = true;
+                            turn = 2;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (middleRight.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                    if (chosenPosition == 7)
+                    {
+                        if (!bottomLeft.occupied)
+                        {
+                            bottomLeft.value = 1;
+                            bottomLeft.occupied = true;
+                            turn = 2;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (bottomLeft.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                    if (chosenPosition == 8)
+                    {
+                        if (!bottomMiddle.occupied)
+                        {
+                            bottomMiddle.value = 1;
+                            bottomMiddle.occupied = true;
+                            turn = 2;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (bottomMiddle.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                    if (chosenPosition == 9)
+                    {
+                       if (!bottomRight.occupied)
+                        {
+                            bottomRight.value = 1;
+                            bottomRight.occupied = true;
+                            turn = 2;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (bottomRight.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                }
 
-                Console.WriteLine("Player 1. Which box would you like to go?");
-                chosenPositionString = Console.ReadLine();
-                chosenPosition = Int32.Parse(chosenPositionString);
-                Console.WriteLine(chosenPosition);
-                // Ok some annoying if statements here we go....
-                if (chosenPosition == 1)
+                // Yay player 2
+
+                if (turn == 2)
                 {
-                    topLeft.chosenTemp = true;
+                    Console.WriteLine("Player 2. Which box would you like to go?");
+                    chosenPositionString = Console.ReadLine();
+                    chosenPosition = Int32.Parse(chosenPositionString);
+                    Console.WriteLine(chosenPosition);
+                    // Ok some annoying if statements here we go....
+                    if (chosenPosition == 1)
+                    {
+                        if (!topLeft.occupied)
+                        {
+                            topLeft.value = 2;
+                            topLeft.occupied = true;
+                            turn = 1;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (topLeft.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                    if (chosenPosition == 2)
+                    {
+                        if (!topMiddle.occupied)
+                        {
+                            topMiddle.value = 2;
+                            topMiddle.occupied = true;
+                            turn = 1;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (topMiddle.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                    if (chosenPosition == 3)
+                    {
+                        if (!topRight.occupied)
+                        {
+                            topRight.value = 2;
+                            topRight.occupied = true;
+                            turn = 1;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (topRight.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                    if (chosenPosition == 4)
+                    {
+                        if (!middleLeft.occupied)
+                        {
+                            middleLeft.value = 2;
+                            middleLeft.occupied = true;
+                            turn = 1;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (middleLeft.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                    if (chosenPosition == 5)
+                    {
+                        if (!middleMiddle.occupied)
+                        {
+                            middleMiddle.value = 2;
+                            middleMiddle.occupied = true;
+                            turn = 1;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (middleMiddle.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                    if (chosenPosition == 6)
+                    {
+                        if (!middleRight.occupied)
+                        {
+                            middleRight.value = 2;
+                            middleRight.occupied = true;
+                            turn = 1;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (middleRight.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                    if (chosenPosition == 7)
+                    {
+                        if (!bottomLeft.occupied)
+                        {
+                            bottomLeft.value = 2;
+                            bottomLeft.occupied = true;
+                            turn = 1;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (bottomLeft.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                    if (chosenPosition == 8)
+                    {
+                        if (!bottomMiddle.occupied)
+                        {
+                            bottomMiddle.value = 2;
+                            bottomMiddle.occupied = true;
+                            turn = 1;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (bottomMiddle.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
+                    if (chosenPosition == 9)
+                    {
+                       if (!bottomRight.occupied)
+                        {
+                            bottomRight.value = 2;
+                            bottomRight.occupied = true;
+                            turn = 1;
+                            displayGrid();
+                            continue;
+                        }
+                        else if (bottomRight.occupied)
+                        {
+                            Console.WriteLine("That spot is already occupied. Please choose somewhere else");
+                            displayGrid();
+                            continue;
+                        }
+                    }
                 }
-                if (chosenPosition == 2)
-                {
-                    topMiddle.chosenTemp = true;
-                }
-                if (chosenPosition == 3)
-                {
-                    topLeft.chosenTemp = true;
-                }
-                if (chosenPosition == 4){
-                    middleLeft.chosenTemp = true;
-                }
-                if (chosenPosition == 5){
-                    middleMiddle.chosenTemp = true;
-                }
-                if (chosenPosition == 6){
-                    middleRight.chosenTemp = true;
-                }
-                if (chosenPosition == 7)
-                {
-                    bottomLeft.chosenTemp = true;
-                }
-                if (chosenPosition == 8){
-                    bottomMiddle.chosenTemp = true;
-                }
-                if (chosenPosition == 9)
-                {
-                    bottomRight.chosenTemp = true;
-                }
+
 
                 // If statements are done!
 
 
                 // Make sure numbers are correctly formatted
-                topLeft.convertLetter();
-                topMiddle.convertLetter();
-                topRight.convertLetter();
-                middleLeft.convertLetter();
-                middleMiddle.convertLetter();
-                middleRight.convertLetter();
-                bottomRight.convertLetter();
-                bottomMiddle.convertLetter();
-                bottomLeft.convertLetter();
-                // Display grid
-                Console.WriteLine("----------");
-                Console.WriteLine($"{topLeft.letter} | {topMiddle.letter} | {topRight.letter}");
-                Console.WriteLine("----------");
-                Console.WriteLine($"{middleLeft.letter} | {middleMiddle.letter} | {middleRight.letter}");
-                Console.WriteLine("----------");
-                Console.WriteLine($"{bottomLeft.letter} | {bottomMiddle.letter} | {bottomRight.letter}");
-                Console.WriteLine("----------");
-                Console.WriteLine();
+
+
+
+
+
+
+
+
+
+                
+                void displayGrid()
+                {
+                    topLeft.convertLetter();
+                    topMiddle.convertLetter();
+                    topRight.convertLetter();
+                    middleLeft.convertLetter();
+                    middleMiddle.convertLetter();
+                    middleRight.convertLetter();
+                    bottomRight.convertLetter();
+                    bottomMiddle.convertLetter();
+                    bottomLeft.convertLetter();
+                    // Display grid
+                    Console.WriteLine("----------");
+                    Console.WriteLine($"{topLeft.letter} | {topMiddle.letter} | {topRight.letter}");
+                    Console.WriteLine("----------");
+                    Console.WriteLine($"{middleLeft.letter} | {middleMiddle.letter} | {middleRight.letter}");
+                    Console.WriteLine("----------");
+                    Console.WriteLine($"{bottomLeft.letter} | {bottomMiddle.letter} | {bottomRight.letter}");
+                    Console.WriteLine("----------");
+                    Console.WriteLine();
+                }
+                Console.WriteLine("Nothing for me to do.");
+                Thread.Sleep(1000);
+
             }
             Console.WriteLine("Press any key to close...");
             Console.ReadKey();
