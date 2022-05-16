c5f62978482ae5ec48fa05f5bb60d6073eac2bde
AAR072 <aliakber@requity.ca>
2022-05-16 11:32:31 -0400

    Updated grid.cs

--- grid.cs
+++ grid.cs
@@ -4,32 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace tictactoe {
-  class grid {
-    // Constructor of the grid
-    public grid(int locationConstructor) {
-      location = locationConstructor;
+namespace tictactoe
+{
+    class grid
+    {
+        // Constructor of the grid
+        public grid(int locationConstructor, bool occupiedConstructor, bool chosenTempConstructor)
+        {
+            location = locationConstructor;
+            occupied = occupiedConstructor;
+        }
+        public int location
+        {
+            get;
+            set;
+        }
+        // Shows Value of the spot. 0 = nothing, 1 = X, 2 = O
+        public int value;
+        public string letter;
+        public bool occupied { get; set; }
+        public string chosenPositionString;
+        public bool chosenTemp;
+        public int chosenPosition;
+        // Letter converter function
+        public void convertLetter()
+        {
+            if (value == 0)
+            {
+                letter = "0";
+
+            }
+            if (value == 1)
+            {
+                letter = "X";
+            }
+            if (value == 2)
+            {
+                letter = "O";
+            }
+        }
+
+
+
+
+
+
+
+        }
+
+
+
+
+
 
     }
-    public int location {
-      get;
-      set;
-    }
-    // Shows Value of the spot. 0 = nothing, 1 = X, 2 = O
-    public int value;
-    public string letter;
-    public void convertLetter() {
-      if (value == 0) {
-        letter = "0";
-
-      }
-      if (value == 1) {
-        letter = "X";
-      }
-      if (value == 2) {
-        letter = "O";
-      }
-    }
-
-  }
 }
